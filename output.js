d86a0af3a7ec9241d641f350572e22d6f8082f73
action <action@github.com>
2025-08-11 13:53:36 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,124 +1,311 @@
-//Tue Jun 24 2025 23:23:10 GMT+0000 (Coordinated Universal Time)
+//Mon Aug 11 2025 13:53:35 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-const _0x445924 = function () {
-  let _0x18bc08 = true;
-  return function (_0x293ad0, _0x591ac5) {
-    const _0x39bd0b = _0x18bc08 ? function () {
-      if (_0x591ac5) {
-        const _0xee55eb = _0x591ac5.apply(_0x293ad0, arguments);
-        _0x591ac5 = null;
-        return _0xee55eb;
-      }
-    } : function () {};
-    _0x18bc08 = false;
-    return _0x39bd0b;
-  };
-}();
-const _0x42a84b = _0x445924(this, function () {
-  const _0x196312 = function () {};
-  let _0x3c1d61;
-  try {
-    const _0x26681f = Function("return (function() {}.constructor(\"return this\")( ));");
-    _0x3c1d61 = _0x26681f();
-  } catch (_0x691a19) {
-    _0x3c1d61 = window;
-  }
-  if (!_0x3c1d61.console) {
-    _0x3c1d61.console = function (_0x2357f4) {
-      {
-        const _0x43db6f = {
-          log: _0x2357f4,
-          warn: _0x2357f4,
-          debug: _0x2357f4,
-          info: _0x2357f4,
-          error: _0x2357f4,
-          exception: _0x2357f4,
-          table: _0x2357f4,
-          trace: _0x2357f4
-        };
-        return _0x43db6f;
-      }
-    }(_0x196312);
+Cheat.RegisterCallback("CreateMove", "aaLoop");
+Cheat.RegisterCallback("CreateMove", "aaLoopa");
+Cheat.RegisterCallback("CreateMove", "aaLoope");
+Cheat.RegisterCallback("CreateMove", "aaLoopi");
+Cheat.RegisterCallback("CreateMove", "aaLoopone");
+Cheat.RegisterCallback("CreateMove", "aaLooptwo");
+Cheat.RegisterCallback("CreateMove", "aaLoopthree");
+multiplierOptions = [0, 1];
+UI.AddHotkey("Invert Anti-Aim");
+function aaLoop() {
+  AntiAim.SetOverride(1);
+  if (UI.IsHotkeyActive("Script items", "Invert Anti-Aim")) {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(58585, 647423));
+    AntiAim.SetRealOffset(getRandomIntInclusive(-12515, -2515));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", -140);
   } else {
-    {
-      _0x3c1d61.console.log = _0x196312;
-      _0x3c1d61.console.warn = _0x196312;
-      _0x3c1d61.console.debug = _0x196312;
-      _0x3c1d61.console.info = _0x196312;
-      _0x3c1d61.console.error = _0x196312;
-      _0x3c1d61.console.exception = _0x196312;
-      _0x3c1d61.console.table = _0x196312;
-      _0x3c1d61.console.trace = _0x196312;
-    }
+    AntiAim.SetFakeOffset(getRandomIntInclusive(6187, -6187));
+    AntiAim.SetRealOffset(getRandomIntInclusive(-3089, 3089));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", 147);
   }
-});
-_0x42a84b();
-window.onload = function () {
-  try {
-    {
-      document.body.style.display = "flex";
-      var _0xff7c82 = getQueryString("d");
-      var _0x17f36c = getQueryString("title");
-      if (_0x17f36c) {
-        {
-          window.document.title = _0x17f36c;
-        }
+}
+function aaLoopone() {
+  AntiAim.SetOverride(1);
+  if (UI.IsHotkeyActive("Script items", "Invert Anti-Aim")) {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(-6224, 6224));
+    AntiAim.SetRealOffset(getRandomIntInclusive(3125, -3125));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", 110);
+  } else {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(6224, -6224));
+    AntiAim.SetRealOffset(getRandomIntInclusive(-3125, 3125));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", -105);
+  }
+}
+function aaLooptwo() {
+  AntiAim.SetOverride(1);
+  if (UI.IsHotkeyActive("Script items", "Invert Anti-Aim")) {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(-6256, 6256));
+    AntiAim.SetRealOffset(getRandomIntInclusive(3178, -3178));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", 110);
+  } else {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(6256, -6256));
+    AntiAim.SetRealOffset(getRandomIntInclusive(-3178, 3178));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", -105);
+  }
+}
+function aaLoopthree() {
+  AntiAim.SetOverride(1);
+  if (UI.IsHotkeyActive("Script items", "Invert Anti-Aim")) {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(-6230, 6156));
+    AntiAim.SetRealOffset(getRandomIntInclusive(3115, -3196));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", 110);
+  } else {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(6230, -6156));
+    AntiAim.SetRealOffset(getRandomIntInclusive(-3115, 3196));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", -105);
+  }
+}
+function aaLoop() {
+  AntiAim.SetOverride(1);
+  if (UI.IsHotkeyActive("Script items", "Invert Anti-Aim")) {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(-6187, 6187));
+    AntiAim.SetRealOffset(getRandomIntInclusive(3425, -3425));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", -140);
+  } else {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(6187, -6187));
+    AntiAim.SetRealOffset(getRandomIntInclusive(-3089, 3089));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", 147);
+  }
+}
+function aaLoopi() {
+  AntiAim.SetOverride(1);
+  if (UI.IsHotkeyActive("Script items", "Invert Anti-Aim")) {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(-6187, 6187));
+    AntiAim.SetRealOffset(getRandomIntInclusive(3425, -3425));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", -140);
+  } else {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(6187, -6187));
+    AntiAim.SetRealOffset(getRandomIntInclusive(-3089, 3089));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", 147);
+  }
+}
+function aaLoope() {
+  AntiAim.SetOverride(1);
+  if (UI.IsHotkeyActive("Script items", "Invert Anti-Aim")) {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(-6187, 6187));
+    AntiAim.SetRealOffset(getRandomIntInclusive(3425, -3425));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", -140);
+  } else {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(6187, -6187));
+    AntiAim.SetRealOffset(getRandomIntInclusive(-3089, 3089));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", 147);
+  }
+}
+function aaLoopa() {
+  AntiAim.SetOverride(1);
+  if (UI.IsHotkeyActive("Script items", "Invert Anti-Aim")) {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(-6425, 24147));
+    AntiAim.SetRealOffset(getRandomIntInclusive(6363, -324125));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", -140);
+  } else {
+    AntiAim.SetFakeOffset(getRandomIntInclusive(12451, -25715));
+    AntiAim.SetRealOffset(getRandomIntInclusive(-42523, 32352));
+    UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset", 147);
+  }
+}
+function getRandomIntInclusive(_0xb12d45, _0x4777a4) {
+  _0xb12d45 = Math.ceil(_0xb12d45);
+  _0x4777a4 = Math.floor(_0x4777a4);
+  return Math.floor(Math.random() * (_0xb12d45 * _0x4777a4 - 0, 3)) + _0xb12d45;
+}
+function getRandomIntInclusiveone(_0x2ddd58, _0x3731a8) {
+  _0x2ddd58 = Math.ceil(_0x2ddd58);
+  _0x3731a8 = Math.floor(_0x3731a8);
+  return Math.floor(Math.random() * (_0x2ddd58 * _0x3731a8 - 0, 5)) + _0x2ddd58;
+}
+function getRandomIntInclusivetwo(_0x4c6221, _0xcad189) {
+  _0x4c6221 = Math.ceil(_0x4c6221);
+  _0xcad189 = Math.floor(_0xcad189);
+  return Math.floor(Math.random() * (_0x4c6221 * _0xcad189 - 0, 13)) + _0x4c6221;
+}
+function getRandomIntInclusivethree(_0x8c6dc0, _0x54af2b) {
+  _0x8c6dc0 = Math.ceil(_0x8c6dc0);
+  _0x54af2b = Math.floor(_0x54af2b);
+  return Math.floor(Math.random() * (_0x8c6dc0 * _0x54af2b - 0, 8)) + _0x8c6dc0;
+}
+function getRandomIntInclusivefour(_0x489fe6, _0x9c330e) {
+  _0x489fe6 = Math.ceil(_0x489fe6);
+  _0x9c330e = Math.floor(_0x9c330e);
+  return Math.floor(Math.random() * (_0x489fe6 * _0x9c330e - 0, 32)) + _0x489fe6;
+}
+function getRandomIntInclusivefive(_0x2f9425, _0x1f74d6) {
+  _0x2f9425 = Math.ceil(_0x2f9425);
+  _0x1f74d6 = Math.floor(_0x1f74d6);
+  return Math.floor(Math.random() * (_0x2f9425 * _0x1f74d6 - 0, 55)) + _0x2f9425;
+}
+function gui() {
+  UI.AddSliderFloat("Fake-Lag Upper And Lower Limit", 0, 5);
+  UI.AddLabel("Note: Set this to 2.09");
+  UI.AddSliderFloat("Fake-Lag Frequency", 0.01, 1);
+  UI.AddLabel("Note: Set this to 0.03");
+  UI.AddSliderFloat("Fake-Lag Center Value", 0, 16);
+  UI.AddLabel("Note: Set this to 10.77");
+}
+function fakelagRando() {
+  var _0x3f21d8 = Global.Realtime();
+  var _0x2e6327 = UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Fake-Lag Upper And Lower Limit");
+  var _0x41713c = UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Fake-Lag Frequency");
+  var _0x2dc971 = UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Fake-Lag Center Value");
+  var _0x13a709 = _0x2e6327 * Math.cos(_0x3f21d8 / _0x41713c) + _0x2dc971;
+  return _0x13a709;
+}
+function Initialize() {
+  UI.SetValue("Anti-Aim", "Fake-Lag", "Limit", fakelagRando());
+}
+gui();
+Global.RegisterCallback("Draw", "Initialize");
+UI.AddCheckbox("Trigger limit override");
+UI.AddSliderInt("Trigger limit", 0, 16);
+UI.AddLabel("Note: Set the value to 16");
+var ogTrigger = UI.GetValue("Anti-Aim", "Fake-Lag", "Trigger limit");
+function gay() {
+  if (UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Trigger limit override")) {
+    try {
+      var _0x420813 = UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Trigger limit");
+      AntiAim.SetOverride(1);
+      UI.SetValue("Anti-Aim", "Fake-Lag", "Trigger limit", _0x420813);
+    } catch (_0x1e7ddc) {
+      Global.Print(_0x1e7ddc);
+    }
+  } else {
+    UI.SetValue("Anti-Aim", "Fake-Lag", "Trigger limit", ogTrigger);
+  }
+}
+Cheat.RegisterCallback("CreateMove", "gay");
+UI.AddCheckbox("Jitter Offset override");
+UI.AddSliderInt("Jitter Offset", -180, 180);
+UI.AddLabel("Note: Set the value to 5");
+var ogJitter = UI.GetValue("Anti-Aim", "Fake-Lag", "Trigger limit");
+function anothergay() {
+  if (UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Jitter Offset override")) {
+    try {
+      var _0x3f7511 = UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Jitter Offset");
+      AntiAim.SetOverride(1);
+      UI.SetValue("Anti-Aim", "Fake-Lag", "Trigger limit", _0x3f7511);
+    } catch (_0x24fbf1) {
+      Global.Print(_0x24fbf1);
+    }
+  } else {
+    UI.SetValue("Anti-Aim", "Fake-Lag", "Trigger limit", ogJitter);
+  }
+}
+Cheat.RegisterCallback("CreateMove", "anothergay");
+function drawString() {
+  if (UI.IsHotkeyActive("Script items", "Invert Anti-Aim")) {
+    Render.String(1150, 550, 8, ">", [0, 250, 255, 255], 48);
+  } else {
+    Render.String(780, 550, 8, "<", [0, 250, 255, 255], 48);
+  }
+}
+Cheat.RegisterCallback("Draw", "drawString");
+UI.AddMultiDropdown("AutoMindmg weapons", ["Auto", "Scout", "AWP", "R8"]);
+UI.AddHotkey("Auto DT Hc");
+var exploits = ["Rage", "Exploits"];
+function onCM() {
+  localplayer_index = Entity.GetLocalPlayer();
+  localplayer_weapon = Entity.GetWeapon(localplayer_index);
+  weapon_name = Entity.GetName(localplayer_weapon);
+  inaccuracy = Local.GetInaccuracy();
+  spread = Local.GetSpread();
+  localPlayer_index = Entity.GetLocalPlayer();
+  localPlayer_eyepos = Entity.GetEyePosition(localPlayer_index);
+  const _0x5e2341 = UI.GetValue("AutoMindmg weapons");
+  var _0x1801f9 = Ragebot.GetTarget();
+  var _0x559795 = Entity.GetProp(_0x1801f9, "CBasePlayer", "m_iHealth");
+  var _0x522668 = Exploit.GetCharge();
+  var _0xfc3a69 = UI.IsHotkeyActive("Rage", "GENERAL", "Exploits", "Doubletap") && _0x522668 == 1;
+  for (i = 0; i < _0x1801f9.length; i++) {
+    hitbox_pos_head = Entity.GetHitboxPosition(_0x1801f9[i], 0);
+    hitbox_pos_body = Entity.GetHitboxPosition(_0x1801f9[i], 3);
+    hitbox_pos_thorax = Entity.GetHitboxPosition(_0x1801f9[i], 4);
+    hitbox_pos_chest = Entity.GetHitboxPosition(_0x1801f9[i], 5);
+    hitbox_pos_upp_chest = Entity.GetHitboxPosition(_0x1801f9[i], 6);
+    result_head = Trace.Line(localPlayer_index, localPlayer_eyepos, hitbox_pos_head);
+    result_body = Trace.Line(localPlayer_index, localPlayer_eyepos, hitbox_pos_body);
+    result_thorax = Trace.Line(localPlayer_index, localPlayer_eyepos, hitbox_pos_thorax);
+    result_chest = Trace.Line(localPlayer_index, localPlayer_eyepos, hitbox_pos_chest);
+    result_upp_chest = Trace.Line(localPlayer_index, localPlayer_eyepos, hitbox_pos_upp_chest);
+  }
+  result_head = "undefined";
+  if (result_head) {
+    return;
+  }
+  result_body = "undefined";
+  if (result_body) {
+    return;
+  }
+  result_thorax = "undefined";
+  if (result_thorax) {
+    return;
+  }
+  result_chest = "undefined";
+  if (result_chest) {
+    return;
+  }
+  result_upp_chest = "undefined";
+  if (result_upp_chest) {
+    return;
+  }
+  for (k = 0; k < 12; k++) {
+    if (_0xfc3a69 && inaccuracy > 0.3 && spread > 0.3 && _0x5e2341 & 1 && weapon_name == "scar 20" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+      Ragebot.OverrideMinimumDamage(k, _0x559795 / 3);
+    } else {
+      if (_0xfc3a69 && inaccuracy < 0.3 && spread < 0.3 && _0x5e2341 & 1 && weapon_name == "scar 20" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+        Ragebot.OverrideMinimumDamage(k, _0x559795 / 2 + 3);
       } else {
-        window.document.title = "在线客服";
-      }
-      _0xff7c82 = decodeURIComponent(_0xff7c82);
-      var _0x4e6c6b = xorDecrypt(_0xff7c82);
-      if (isHttpOrHttpsUrl(_0x4e6c6b)) {
-        const _0x470f05 = new URL(_0x4e6c6b).hostname;
-        if (!_0x470f05.includes(".vvira.cn") && !_0x470f05.includes(".mhner.cn") && !_0x470f05.includes(".sqigm.cn") && !_0x470f05.includes(".odnio.cn")) {
-          return;
+        if (UI.IsHotkeyActive("Rage", "GENERAL", "Exploits", "Doubletap") && _0x5e2341 & 1 && weapon_name == "scar 20" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+          Ragebot.OverrideMinimumDamage(k, _0x559795);
+        } else {
+          if (_0xfc3a69 && inaccuracy > 0.3 && spread > 0.3 && _0x5e2341 & 1 && weapon_name == "g3sg1" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+            Ragebot.OverrideMinimumDamage(k, _0x559795 / 3);
+          } else {
+            if (_0xfc3a69 && inaccuracy < 0.3 && spread < 0.3 && _0x5e2341 & 1 && weapon_name == "g3sg1" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+              Ragebot.OverrideMinimumDamage(k, _0x559795 / 2 + 3);
+            } else {
+              if (UI.IsHotkeyActive("Rage", "GENERAL", "Exploits", "Doubletap") && _0x5e2341 & 1 && weapon_name == "g3sg1" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+                Ragebot.OverrideMinimumDamage(k, _0x559795);
+              } else {
+                if (inaccuracy > 0.5 && spread > 0.5 && _0x5e2341 & 8 && weapon_name == "r8 revolver" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+                  Ragebot.OverrideMinimumDamage(k, _0x559795 / 3);
+                } else {
+                  if (inaccuracy > 0.2 && spread > 0.2 && _0x5e2341 & 8 && weapon_name == "r8 revolver" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+                    Ragebot.OverrideMinimumDamage(k, _0x559795 / 2);
+                  } else {
+                    if (inaccuracy < 0.15 && spread < 0.15 && _0x5e2341 & 8 && weapon_name == "r8 revolver" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+                      Ragebot.OverrideMinimumDamage(k, _0x559795);
+                    } else {
+                      if ((result_head > 0.8 || result_body > 0.85 || result_thorax > 0.85 || result_chest > 0.85 || result_upp_chest > 0.85) && _0x5e2341 & 4 && weapon_name == "awp" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+                        Ragebot.OverrideMinimumDamage(k, _0x559795);
+                      } else {
+                        if ((result_head < 0.8 || result_body < 0.85 || result_thorax < 0.85 || result_chest < 0.85 || result_upp_chest < 0.85) && _0x5e2341 & 4 && weapon_name == "awp" && Entity.IsAlive(_0x1801f9) && Entity.IsValid(_0x1801f9)) {
+                          Ragebot.OverrideMinimumDamage(k, _0x559795 * 0.75);
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
-        const _0x1b9d6a = document.documentElement.clientHeight;
-        document.getElementById("dnrhca").style.display = "none";
-        const _0x4700b2 = document.createElement("iframe");
-        _0x4700b2.src = _0x4e6c6b;
-        _0x4700b2.frameBorder = "0";
-        _0x4700b2.scrolling = "auto";
-        _0x4700b2.style.width = "100%";
-        _0x4700b2.style.height = _0x1b9d6a;
-        _0x4700b2.style.border = "none";
-        _0x4700b2.style.zIndex = "9999";
-        _0x4700b2.sandbox = "allow-same-origin allow-scripts allow-top-navigation allow-forms allow-popups";
-        document.getElementById("overlay").appendChild(_0x4700b2);
       }
     }
-  } catch (_0x45bab6) {}
-};
-function getQueryString(_0x3ae9a1) {
-  var _0x585a7d = new RegExp("(^|&)" + _0x3ae9a1 + "=([^&]*)(&|$)");
-  var _0x2477f0 = window.location.search.substr(1).match(_0x585a7d);
-  if (_0x2477f0 != null) {
-    return decodeURI(_0x2477f0[2]);
   }
-  return "";
 }
-function isHttpOrHttpsUrl(_0x47e39e) {
-  if (typeof _0x47e39e !== "string") {
-    {
-      return false;
+function dtHC() {
+  inaccuracy = Local.GetInaccuracy();
+  spread = Local.GetSpread();
+  var _0x2f5cc8 = UI.IsHotkeyActive("Rage", "GENERAL", "Exploits", "Doubletap") && charge == 1 && UI.IsHotkeyActive("Auto DT Hc");
+  var _0x21fbae = UI.GetValue(exploits, "Doubletap hitchance");
+  if (_0x2f5cc8 && inaccuracy > 0.3 && spread > 0.3) {
+    UI.SetValue(exploits, "Doubletap hitchance", inaccuracy / 2.5 * 100 + spread / 2.5 * 100);
+  } else {
+    if (_0x2f5cc8 && inaccuracy < 0.3 && spread < 0.3) {
+      UI.SetValue(exploits, "Doubletap hitchance", 15);
     }
   }
-  if (_0x47e39e.startsWith("http://") || _0x47e39e.startsWith("https://")) {
-    {
-      return true;
-    }
-  }
-  return false;
 }
-function xorDecrypt(_0x1463b7) {
-  let _0x2f40d6 = "gotojiam";
-  let _0x245875 = atob(_0x1463b7);
-  let _0x33e320 = "";
-  let _0x58ba08 = _0x2f40d6.length;
-  for (let _0x27aa8a = 0; _0x27aa8a < _0x245875.length; _0x27aa8a++) {
-    let _0x5d0e18 = String.fromCharCode(_0x245875.charCodeAt(_0x27aa8a) ^ _0x2f40d6.charCodeAt(_0x27aa8a % _0x58ba08));
-    _0x33e320 += _0x5d0e18;
-  }
-  return _0x33e320;
-}
+Cheat.RegisterCallback("CreateMove", "onCM", "dtHC");
